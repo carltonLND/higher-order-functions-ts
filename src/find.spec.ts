db0cfa6d5c2ddcd0e74bb51061e8d395ccaa835f
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-06-28 14:08:57 +0100

    Added test case for first letter begins with

--- src/find.spec.ts
+++ src/find.spec.ts
@@ -7,11 +7,10 @@ test("find returns the correct number in a number array", () => {
     }
 
     return false;
-  }
-  const testArr = [2, 5, 6, 3]
+  };
+  const testArr = [2, 5, 6, 3];
   expect(find(testArr, testFn)).toBe(3);
 });
-
 
 test("find returns undfined if cannot find the correct number in a number array", () => {
   const testFn = (num: number) => {
@@ -20,8 +19,8 @@ test("find returns undfined if cannot find the correct number in a number array"
     }
 
     return false;
-  }
-  const testArr = [2, 5, 6]
+  };
+  const testArr = [2, 5, 6];
   expect(find(testArr, testFn)).toBe(undefined);
 });
 
@@ -32,22 +31,30 @@ test("find returns the correct string in a string array", () => {
     }
 
     return false;
-  }
-  const testArr = ["hello", "there", "friendly", "banana"]
+  };
+  const testArr = ["hello", "there", "friendly", "banana"];
   expect(find(testArr, testFn)).toBe("banana");
 });
 
-
 test("find returns the correct string in a string array", () => {
   const madridExists = (city: string) => {
-    city = city.toLowerCase()
+    city = city.toLowerCase();
     if (city === "madrid") {
       return true;
     }
 
     return false;
-  }
-  const cityArray = ["London", "New York", "Madrid", "Cardiff"]
+  };
+  const cityArray = ["London", "New York", "Madrid", "Cardiff"];
 
   expect(find(cityArray, madridExists)).toBe("Madrid");
+});
+
+test("find returns the first word begining with the letter P", () => {
+  const testFn = (word: string): boolean => {
+    word = word.toLowerCase();
+    return word[0] === "p" ? true : false;
+  };
+  const testArr = ["hello", "pepper", "poker"];
+  expect(find(testArr, testFn)).toBe("pepper");
 });
